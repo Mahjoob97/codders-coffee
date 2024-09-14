3ab24c32867a148f7027fc144f5717647828a0dd
Mahjoob97 <mahjoobalhaj4@gmail.com>
2024-09-15 01:41:58 +0300

    completed hero section and added the nav section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import BgImage from '../assets/bg-slate.png'
+import TheMug from '../assets/Black.png'
+import Navbar from './Navbar'
+import {motion} from 'framer-motion'
 
 const bgImage = {
     backgroundImage: `url(${BgImage})`, 
@@ -13,32 +16,57 @@ function Hero() {
     <main style={bgImage}>
         <section className='min-h-[750px] w-full'>
             <div className="container">
-
-                <div className="grid grid-cols-1 md:grid-cols-2">
+                {/* navbar section */}
+                <Navbar/>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center min-h-[850px] ">
                     {/* text content section */}
-                    <div className='text-lightOrange'>
+                    <div className='text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28'>
                         <h1 className='text-7xl font-bold leading-tight ml-14'>Blvck Tumbler</h1>
+                        <div className='relative'>
+                            <div className='relative space-y-4 z-10'>
+                                <h1 className='text-2xl'>
+                                    Black Lovers Lifestyle, 
+                                </h1>
+                                <h1 className='text-sm opacity-55 leading-loose'>
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                                    Incidunt asperiores sapiente possimus inventore vel et enim accusantium, 
+                                    ab praesentium atque provident est tenetur quasi impedit, 
+                                    dicta molestias saepe, necessitatibus iusto?
+                                </h1>
+                            </div>
+                            <div className='absolute -top-6 -left-10 w-[250px] h-[190px] bg-gray-700/25'></div>
+                        </div>
                     </div>
-                    <div>
-                        <div>
-                            <h1>
-                                Black Lovers Lifestyle, 
-                            </h1>
-                            <h1>
-                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                                Incidunt asperiores sapiente possimus inventore vel et enim accusantium, 
-                                ab praesentium atque provident est tenetur quasi impedit, 
-                                dicta molestias saepe, necessitatibus iusto?
+                    
+                    {/* hero image section */}
+                    <div className='relative'>
+                        <img src={TheMug} alt="blackMug" className='relative z-40 h-[400px] md:h-[700px] img-shadow' />
+                        {/* Orange circle */}
+                        <div className='absolute top-24 -right-16 border-primary rounded-full h-[180px] w-[180px] z-10 border-[20px] '></div>
+
+                        {/* big text section */}
+                        <div className='absolute -top-20 left-[200px] z-[1]'>
+                            <h1 className="text-[140px] sclae-150 font-bold text-darkGray/40 leading-none">
+                                Black Tumbler
                             </h1>
                         </div>
                     </div>
-                    {/* hero image section */}
-                    <div>
 
-                    </div>
                     {/* third div section */}
-                    <div>
-
+                    <div className='text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28'>
+                        <h1 className=' opacity-0 text-7xl font-bold leading-tight ml-14'>Blvck Tumbler</h1>
+                        <div className='relative'>
+                            <div className='relative space-y-4 z-10'>
+                                <h1 className='text-2xl'>
+                                    Blvck Tumbler
+                                </h1>
+                                <h1 className='text-sm opacity-55 leading-loose'>
+                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                                    Incidunt asperiores sapiente possimus inventore vel et enim accusantium, 
+                                </h1>
+                            </div>
+                            <div className='absolute -top-6 -right-10 w-[250px] h-[190px] bg-darkGray/50'></div>
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {GiHamburgerMenu} from 'react-icons/gi'
+
+function Navbar() {
+  return (
+    <nav className='absolute top-0 left-0 w-full pt-10 text-white z-20'>
+        <div className="container">
+            <div className='flex justify-between items-center'>
+                {/* logo section */}
+                <h1 className='text-2xl font-semibold uppercase'>
+                    <span className='text-primary'>Coders</span> Caffee
+                </h1>
+                {/* Burger section */}
+                <div>
+                    <GiHamburgerMenu className='text-3xl cursor-pointer'/>
+                </div> 
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
