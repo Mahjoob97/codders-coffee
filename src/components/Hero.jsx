00a985df17e14c3539bd1c1b1ec836138c4de91e
Mahjoob97 <mahjoobalhaj4@gmail.com>
2024-09-15 19:33:11 +0300

    completed sidebar animated section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react'
 import BgImage from '../assets/bg-slate.png'
 import TheMug from '../assets/Black.png'
 import Navbar from './Navbar'
 import {motion} from 'framer-motion'
+import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa6'
 
 const bgImage = {
     backgroundImage: `url(${BgImage})`, 
@@ -12,17 +14,23 @@ const bgImage = {
 }
 
 function Hero() {
+    const [sidebar, setSidebar] = useState(false)
   return (
     <main style={bgImage}>
-        <section className='min-h-[750px] w-full'>
+        <section className='relative min-h-[750px] w-full'>
             <div className="container">
                 {/* navbar section */}
-                <Navbar/>
+                <Navbar sidebar={sidebar} setSidebar={setSidebar}/>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center min-h-[850px] ">
                     {/* text content section */}
                     <div className='text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28'>
-                        <h1 className='text-7xl font-bold leading-tight ml-14'>Blvck Tumbler</h1>
-                        <div className='relative'>
+                        <motion.h1 initial={{opacity: 0, y: -100 }} 
+                        animate={{opacity: 1, y: 0}} 
+                        transition={{type: 'spring', stiffness: 100, damping: 10, delay: 1}}
+                        className='text-7xl font-bold leading-tight ml-14'>Blvck Tumbler</motion.h1>
+                        <motion.div initial={{opacity: 0, y: -100 }} 
+                        animate={{opacity: 1, y: 0}} 
+                        transition={{type: 'spring', stiffness: 100, damping: 10, delay: 1.2}} className='relative'>
                             <div className='relative space-y-4 z-10'>
                                 <h1 className='text-2xl'>
                                     Black Lovers Lifestyle, 
@@ -35,25 +43,38 @@ function Hero() {
                                 </h1>
                             </div>
                             <div className='absolute -top-6 -left-10 w-[250px] h-[190px] bg-gray-700/25'></div>
-                        </div>
+                        </motion.div>
                     </div>
                     
                     {/* hero image section */}
                     <div className='relative'>
-                        <img src={TheMug} alt="blackMug" className='relative z-40 h-[400px] md:h-[700px] img-shadow' />
+                        <motion.img initial={{opacity: 0, scale: 0 }} 
+                            animate={{opacity: 1, scale: 1}} 
+                            transition={{type: 'spring', stiffness: 100, damping: 10, delay: 0.4}} 
+                            src={TheMug} alt="blackMug" className='relative z-40 h-[400px] md:h-[700px] img-shadow' />
                         {/* Orange circle */}
-                        <div className='absolute top-24 -right-16 border-primary rounded-full h-[180px] w-[180px] z-10 border-[20px] '></div>
+                        <motion.div initial={{opacity: 0, y: 100 }} 
+                            animate={{opacity: 1, y: 0}} 
+                            transition={{type: 'spring', stiffness: 100, damping: 10, delay: 0.8}} 
+                            className='absolute top-24 -right-16 border-primary rounded-full h-[180px] w-[180px] z-10 border-[20px] '>
+                        </motion.div>
 
                         {/* big text section */}
-                        <div className='absolute -top-20 left-[200px] z-[1]'>
+                        <motion.div initial={{opacity: 0, x: -100 }} 
+                            animate={{opacity: 1, x: 0}} 
+                            transition={{type: 'spring', stiffness: 100, damping: 10, delay: 0.8}} 
+                            className='absolute -top-20 left-[200px] z-[1]'>
                             <h1 className="text-[140px] sclae-150 font-bold text-darkGray/40 leading-none">
                                 Black Tumbler
                             </h1>
-                        </div>
+                        </motion.div>
                     </div>
 
                     {/* third div section */}
-                    <div className='text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28'>
+                    <motion.div initial={{opacity: 0, y: -100 }} 
+                        animate={{opacity: 1, y: 0}} 
+                        transition={{type: 'spring', stiffness: 100, damping: 10, delay: 1.2}} 
+                        className='text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28'>
                         <h1 className=' opacity-0 text-7xl font-bold leading-tight ml-14'>Blvck Tumbler</h1>
                         <div className='relative'>
                             <div className='relative space-y-4 z-10'>
@@ -67,9 +88,38 @@ function Hero() {
                             </div>
                             <div className='absolute -top-6 -right-10 w-[250px] h-[190px] bg-darkGray/50'></div>
                         </div>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
+            {/* sidebar menu section */}
+            {sidebar && (
+                <motion.div initial={{x: '100%'}}
+                    whileInView={{x: 0}} 
+                    className='absolute top-0 right-0 w-[140px] h-full bg-gradient-to-b from-primary/80 to-primaryDark/80 backdrop-blur-sm z-10'>
+                    <div className='w-full h-full flex justify-center items-center'>
+                        <div className='flex flex-col justify-center items-center gap-6 text-white'>
+                            {/* Line */}
+                            <div className='w-[1px] h-[70px] bg-white'>
+                            </div>
+                            {/* social icons */}
+                            <div className='inline-block p-2 rounded-full cursor-pointer border border-white'>
+                                <FaFacebookF className='text-2xl'/>
+                            </div>
+                            <div className='inline-block p-2 rounded-full cursor-pointer border border-white'>
+                                <FaTwitter className='text-2xl'/>
+                            </div>
+                            <div className='inline-block p-2 rounded-full cursor-pointer border border-white'>
+                                <FaInstagram className='text-2xl'/>
+                            </div>
+                            
+
+                            <div className='w-[1px] h-[70px] bg-white'>
+                            </div>
+                        </div>
+                    </div>
+                </motion.div>
+            )}
+            
         </section>
     </main>
   )
