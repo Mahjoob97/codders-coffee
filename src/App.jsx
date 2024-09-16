1ff072aa1b2ff60afbb17a8b185526da6003ef3f
Mahjoob97 <mahjoobalhaj4@gmail.com>
2024-09-16 20:12:54 +0300

    completed coffee cords section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
+import Service from './components/Service'
 
 function App() {
   return (
     <div className='overflow-x-hidden'>
       <Hero/>
+      <Service />
     </div>
   )
 }
